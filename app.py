43109c18ca17afe967f2dd8ec3f5f88c829ca458
leorprogram <leorsuporte@gmail.com>
2025-07-17 12:19:04 -0300

    app.py

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
 
 db = SQLAlchemy(app)
 mail = Mail(app)
