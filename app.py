12d3156486d0975c448bd203ab811ea023bd2569
leorprogram <leorsuporte@gmail.com>
2025-07-17 12:12:04 -0300

    app.py

--- app.py
+++ app.py
@@ -66,5 +66,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
